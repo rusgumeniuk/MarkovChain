a148693fd9a6d4f3ef3deaafca3d316057903db6
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-03-31 09:26:04 +0300

    init

---
+++ TimeHomogeneousChain/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace TimeHomogeneousChain
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task task = new Task(4, 0, 3, new decimal[,]
+            {
+                {0.1m, 0.2m, 0.3m, 0.4m},
+                { 0, 0.3m,0.2m,0.5m},
+                {0,0,0.4m,0.6m},
+                {0,0,0,1}
+            });
+            task.Solve();
+            MessageBox.Show(task.GetResult());
+        }
+    }
+}
---
+++ TimeHomogeneousChain/Task.cs
@@ -0,0 +1,73 @@
+﻿using QueueingTheoryLibrary;
+
+using System;
+using System.Linq;
+using System.Text;
+
+namespace TimeHomogeneousChain
+{
+    internal class Task : ITask
+    {
+        private readonly ushort IndexOfStartState;
+        private readonly ushort CountOfStates;
+        private readonly ushort NumberOfSteps;
+        private readonly decimal[,] ProbabilityOfTransition;
+        private readonly decimal[,] ProbabilityOfStates;
+        public bool IsSolved { get; set; }
+
+        public Task(ushort countOfStates, ushort indexOfStartState, ushort countOfSteps, decimal[,] probabilitiesOfTransition)
+        {
+            CountOfStates = countOfStates;
+            IndexOfStartState = indexOfStartState;
+            NumberOfSteps = countOfSteps;
+            ProbabilityOfTransition = probabilitiesOfTransition;
+            ProbabilityOfStates = new decimal[NumberOfSteps, CountOfStates];
+        }
+
+        public string GetResult()
+        {
+            StringBuilder resultText = new StringBuilder();
+            resultText.AppendLine("Ймовірності перебування системи в станах:");
+            for (int i = 0; i < CountOfStates; ++i)
+            {
+                resultText.AppendLine($"Стан №{i + 1}: {ProbabilityOfStates[NumberOfSteps - 1, i]}");
+            }
+            return resultText.ToString();
+        }
+
+        public void Solve()
+        {
+            for (int indexOfStep = 0; indexOfStep < NumberOfSteps; ++indexOfStep)
+            {
+                decimal[] row = CalculateProbabilitiesOfStates(indexOfStep);
+                for (int indexOfState = 0; indexOfState < CountOfStates; ++indexOfState)
+                {
+                    ProbabilityOfStates[indexOfStep, indexOfState] = row[indexOfState];
+                }
+            }
+        }
+
+        private decimal[] CalculateProbabilitiesOfStates(int indexOfStep)
+        {
+            decimal[] array = new decimal[CountOfStates];
+            if (indexOfStep == 0)
+            {
+                for (int i = 0; i < CountOfStates; ++i)
+                {
+                    array[i] = ProbabilityOfTransition[IndexOfStartState, i];
+                }
+            }
+            else
+            {
+                for (int indexOfCurrentState = 0; indexOfCurrentState < CountOfStates; ++indexOfCurrentState)
+                {
+                    for (int indexOfPreviousState = 0; indexOfPreviousState < CountOfStates; indexOfPreviousState++)
+                    {
+                        array[indexOfCurrentState] += ProbabilityOfStates[indexOfStep - 1, indexOfPreviousState] * ProbabilityOfTransition[indexOfPreviousState, indexOfCurrentState];
+                    }
+                }
+            }
+            return array.Sum() == 1 ? array : throw new ArithmeticException($"Сума імовірностей станів на кроці {indexOfStep} не дорівнює 1!");
+        }
+    }
+}
