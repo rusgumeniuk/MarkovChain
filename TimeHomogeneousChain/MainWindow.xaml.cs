7d4cb6e14303c6eb77367553274934bcef62e820
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-04-01 20:41:18 +0300

    Created class State (to simplify work with DataGrid). Implemented method GenerateTransitionTable(), added method GetTable() which allow to parse and format 2d array from datagrid. Added command on click to BtnClear. Added prop StartIndex

--- TimeHomogeneousChain/MainWindow.xaml.cs
+++ TimeHomogeneousChain/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace TimeHomogeneousChain
 {
@@ -7,9 +11,21 @@ namespace TimeHomogeneousChain
     /// </summary>
     public partial class MainWindow : Window
     {
-        private uint countOfStates = 4;
-        private uint numberOfSteps = 3;
-        public uint CountOfStates
+        private ushort countOfStates = 1;
+        private ushort numberOfSteps = 1;
+        private ushort startIndex;
+        public ushort StartIndex
+        {
+            get => (ushort)(startIndex + 1);
+            set
+            {
+                if (value < 1 || value > countOfStates)
+                    MessageBox.Show("Будь ласка виберіть коректний номер початкового стану!");
+                else
+                    startIndex = (ushort)(value - 1);
+            }
+        }
+        public ushort CountOfStates
         {
             get => countOfStates;
             set
@@ -23,7 +39,7 @@ namespace TimeHomogeneousChain
                     MessageBox.Show("Будь ласка введіть кількість станів системи.\nЧисло повинно бути бути більше нуля!");
             }
         }
-        public uint NumberOfSteps
+        public ushort NumberOfSteps
         {
             get => numberOfSteps;
             set
@@ -41,22 +57,78 @@ namespace TimeHomogeneousChain
             DataContext = this;
         }
 
-        private void GenerateTransitionTable(uint countOfStates)
+        private void GenerateTransitionTable(ushort countOfStates)
         {
+            TransitionTable.Columns.Clear();
 
+            List<State> stateList = new List<State>();
+            for (int i = 0; i < countOfStates; i++)
+            {
+                stateList.Add(new State(i + 1, countOfStates));
+                var col = new DataGridTextColumn() { Header = i + 1 };
+                var binding = new Binding("Values[" + i + "]");
+                col.Binding = binding;
+                TransitionTable.Columns.Add(col);
+            }
+
+            TransitionTable.ItemsSource = stateList;
         }
 
         private void BtnCalculateStatesProbabilities_Click(object sender, RoutedEventArgs e)
         {
-            Task task = new Task(4, 0, 3, new decimal[,]
+            try
             {
-                {0.1m, 0.2m, 0.3m, 0.4m},
-                { 0, 0.3m,0.2m,0.5m},
-                {0,0,0.4m,0.6m},
-                {0,0,0,1}
-            });
-            task.Solve();
-            MessageBox.Show(task.GetResult());
+                Task task = new Task(countOfStates, startIndex, NumberOfSteps, GetTable());
+                task.Solve();
+                MessageBox.Show(task.GetResult());
+                //    {0.1m, 0.2m, 0.3m, 0.4m},
+                //    { 0, 0.3m,0.2m,0.5m},
+                //    {0,0,0.4m,0.6m},
+                //    {0,0,0,1}                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private decimal[,] GetTable()
+        {
+            decimal[,] table = new decimal[countOfStates, countOfStates];
+            for (int i = 0; i < TransitionTable.Items.Count; ++i)
+            {
+                decimal rowValue = 0;
+                for (int j = 0; j < TransitionTable.Columns.Count; ++j)
+                {
+                    var cell = (TransitionTable.Items[i] as State).Values[j];
+                    rowValue += cell;
+                    table[i, j] = cell;
+                }
+                if (rowValue != 1)
+                    throw new ArithmeticException($"Сума ймовірностей переходу із стану №{i + 1} не дорівнює 1!\n Введено некоректні дані. Будь ласка спробуйте ще раз.\nЗверніть увагу, правильний формат запису - '0.25'");
+            }
+            return table;
+        }
+
+        private void BtnClearTable_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateTransitionTable(CountOfStates);
+        }
+
+        private void BtnRandomFillTable_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    internal class State
+    {
+        public int Index { get; set; }
+        public decimal[] Values { get; set; }
+        public State(int index, uint countOfStates)
+        {
+            this.Index = index;
+            Values = new decimal[countOfStates];
         }
     }
 }
